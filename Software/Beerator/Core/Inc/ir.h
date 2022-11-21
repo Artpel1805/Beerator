7d284b5c621f0e4871fcce9d7faab484566c53e6
Artpel1805 <arthurpellegrin1104@gmail.com>
2022-11-21 08:47:32 +0100

    config

---
+++ Software/Beerator/Core/Inc/ir.h
@@ -0,0 +1,13 @@
+/*
+ * ir.h
+ *
+ *  Created on: 21 nov. 2022
+ *      Author: arthurpellegrin
+ */
+
+#ifndef INC_IR_H_
+#define INC_IR_H_
+
+
+
+#endif /* INC_IR_H_ */
---
+++ Software/Beerator/Core/Src/ir.c
@@ -0,0 +1,8 @@
+/*
+ * ir.c
+ *
+ *  Created on: 21 nov. 2022
+ *      Author: arthurpellegrin
+ */
+
+
